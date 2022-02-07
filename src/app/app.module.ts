8503a2f52725a47bc14db46a7bcc28ad522deb87
Aleks <a.bystranowska@gmail.com>
2022-02-07 16:32:02 -0500

    working with modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EmployeelistComponent } from './employeelist/employeelist.component';
+import { Routes } from '@angular/router';
 
+const routes: Routes = [
+  { path: '', component: LoginComponent, pathMatch: 'full'},
+  { path: 'employee', component: EmployeelistComponent, pathMatch: 'full'},
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmployeelistComponent
   ],
   imports: [
     BrowserModule,
